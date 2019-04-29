031a45493520c4fd82e4bc5d9ad635e6b42eb00c
yangjiegeng <yangjiegeng@wacai.com>
2019-04-29 15:46:54 +0800

    feat:springboot整合redis入门

--- src/main/java/com/company/project/entity/User.java
+++ src/main/java/com/company/project/entity/User.java
@@ -27,12 +27,16 @@ public class User implements Serializable {
   private Integer uId;
   private String name;
   private Integer age;
+  // JSON.toJSONString(user)的时候默认是用 字段名 --> name --> alternateNames
+  //  @JSONField(name = "testFieldName",alternateNames = "testFieldAlternateName")
   private String testField;
   /**
    *  注意这里不要使用 isStudent 这样的boolean字段。避免序列化/反序列化找不到该字段
    *  因为 isStudent 和 student 的 get set方法都是一样的
    */
   private boolean studentIs;
+
+  //  @JSONField(format="yyyy-MM-dd HH:mm:ss") //JSON.toJSONString(user)的时候按照指定格式，格式化时间;
   private Date birthday;
 
 }
